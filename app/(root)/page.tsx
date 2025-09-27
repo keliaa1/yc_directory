35b6ce7ab5c94bd89c325d7802b9c5be2770d191
Kelia Simbi <simbikelia@gmail.com>
2025-09-27 20:58:15 +0200

    Feat: Hero-section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h2 className="text-2xl">Home</h2>
+    <section className="pink_container">
+    <h2 className="heading">Pitch Your Startup Idea <br/> Get money, and Launch Your Dream Product in Just 30 Days!
+    </h2>
+    <p className="sub-heading !max-w-3xl">Submit your ideas and gain traction.</p>
+    
+    </section>
     </>
   );
 }
